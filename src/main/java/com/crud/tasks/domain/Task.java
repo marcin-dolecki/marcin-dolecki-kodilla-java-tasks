127046ca9b156bcc4c7b0b5a1e5a155af176a1f5
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-07-01 23:52:55 +0200

    Connection configuration and first postman call to db completed

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,12 +1,27 @@
 package com.crud.tasks.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.EnableMBeanExport;
 
-@Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
