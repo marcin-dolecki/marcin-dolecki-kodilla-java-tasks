7c0cf034a11873488ea4b4a6726169a5a67d3b96
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-16 21:10:19 +0200

    email schedule changed

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -18,7 +18,7 @@ public class EmailScheduler {
     private static final String SUBJECT = "Tasks: Once a day email";
 
 //    @Scheduled(cron = "0 0 10 * * *") // every day at 10
-    @Scheduled(fixedDelay = 10000) // every 10 seconds
+//    @Scheduled(fixedDelay = 10000) // every 10 seconds
     public void sendInformationEmail() {
 
         long size = taskRepository.count();
