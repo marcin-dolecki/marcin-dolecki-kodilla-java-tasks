310c1994b67d81ef03e13fcad94cf8a1f6c99f30
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-09-10 23:06:01 +0200

    filter added in facade

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -1,22 +1,33 @@
 package com.crud.tasks.trello.facade;
 
 import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
 @Component
 @RequiredArgsConstructor
 public class TrelloFacade {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloFacade.class);
     private final TrelloService trelloService;
     private final TrelloMapper trelloMapper;
 
-    public List<TrelloBoard> fetchTrelloBoards() {
-        return trelloMapper.mapToTrelloBoards(trelloService.fetchTrelloBoards());
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        List<TrelloBoard> trelloBoards = trelloMapper.mapToTrelloBoards(trelloService.fetchTrelloBoards());
+        LOGGER.info("Current list size: {}. Starting filtering boards...", trelloBoards.size());
+        List<TrelloBoard> filteredBoards = trelloBoards.stream()
+                .filter(trelloBoard -> !trelloBoard.getName().equalsIgnoreCase("test"))
+                .toList();
+        LOGGER.info("Boards have been filtered. Current list size: {}.", filteredBoards.size());
+        return trelloMapper.mapToTrelloBoardsDto(filteredBoards);
     }
 }
