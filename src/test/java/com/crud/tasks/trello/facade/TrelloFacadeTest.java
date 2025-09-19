aec429867d21e18ab5411695d900bc40861d17ae
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-09-20 01:56:45 +0200

    code coverage in progress

---
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.trello.facade;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloList;
+import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.mapper.TrelloMapper;
+import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.validator.TrelloValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class TrelloFacadeTest {
+
+    @InjectMocks
+    private TrelloFacade trelloFacade;
+
+    @Mock
+    private TrelloService trelloService;
+
+    @Mock
+    private TrelloValidator trelloValidator;
+
+    @Mock
+    private TrelloMapper trelloMapper;
+
+    @Test
+    void shouldFetchEmptyList() {
+        //Given
+        List<TrelloListDto> trelloLists = List.of(new TrelloListDto("1", "test_list", false));
+        List<TrelloBoardDto> trelloBoards = List.of(new TrelloBoardDto("1", "test", trelloLists));
+        List<TrelloList> mappedTrelloList = List.of(new TrelloList("1", "test_list", false));
+        List<TrelloBoard> mappedTrelloBoard = List.of(new TrelloBoard("1", "test", mappedTrelloList));
+
+        when(trelloService.fetchTrelloBoards()).thenReturn(trelloBoards);
+
+    }
+}
