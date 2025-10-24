dd8cda32965d8ba355987d9bbde75be7063ac2c7
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-10-24 23:35:06 +0200

    task 30.1 math operations with thymeleaf

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -9,8 +9,10 @@ import java.util.Map;
 public class StaticWebPageController {
 
     @RequestMapping("/")
-    public String index(Map<String, String > model) {
+    public String index(Map<String, Object> model) {
         model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
         return "index";
     }
 }
