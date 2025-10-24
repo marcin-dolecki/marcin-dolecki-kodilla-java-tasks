cac10f71055e633b8d4fdf8360ec7c93fb5852c4
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-10-25 01:01:49 +0200

    e-mail template in progress - button added

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -16,6 +16,8 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost/tasks_frontend/");
+        context.setVariable("button", "Visit website");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
